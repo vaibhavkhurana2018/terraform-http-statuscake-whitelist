028d4f4dd05010147539c6082024c3f055691909
Vaibhav Khurana <vaibhav.khurana@razorpay.com>
2019-07-04 16:34:32 +0530

    Fixing list format

--- output.tf
+++ output.tf
@@ -1,9 +1,9 @@
 locals {
   ssl_ips = ["162.243.141.135","107.170.235.240","104.236.163.90"]
-  ips  = ["${concat(split(" ", chomp(data.http.list-txt.body)), local.ssl_ips)}"]
+  ips  = ["${concat(list(split(" ", chomp(data.http.list-txt.body))), local.ssl_ips)}"]
 }
 
 output "ips" {
-  description = "List of all StatusCake egress IPs."
+  description = "List of all StatusCake egress IPs appended with subnet"
   value       = ["${formatlist("%s/32", local.ips)}"]
 }
